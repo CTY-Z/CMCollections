7298cdc2187f8ede0736181d13671bdcfa8141b5
zzj <1124981410@qq.com>
2025-11-23 01:29:26 +0800

    ListExtension
    -MoveTemp
    -RemoveSingle
    -IsValidIndex

--- ListExtension.cs
+++ ListExtension.cs
@@ -14,6 +14,32 @@ namespace CMFramework.Core.Collections
             list.Add(item);
             return true;
         }
+
+        public static void MoveTemp<T>(this List<T> target, List<T> other)
+        {
+            if (target == null || other == null || target == other)
+                return;
+
+            target.Clear();
+            target.AddRange(other);
+            other.Clear();
+        }
+
+        public static int RemoveSingle<T>(this List<T> list, T item)
+        {
+            int index = list.IndexOf(item);
+            if (index >= 0)
+            {
+                list.RemoveAt(index);
+            }
+            return index;
+        }
+
+        public static bool IsValidIndex<T>(this List<T> list, int idx)
+        {
+            if (list == null) return false;
+            return idx < 0 || idx >= list.Count;
+        }
     }
 }
     
